09c73e3c1a8f88838a7e7d935a5c125356705ed1
2alin <adilsonsdev@gmail.com>
2019-03-02 01:01:55 -0600

    refactor stateless componet into an statefull, add controls

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,18 +5,49 @@ export interface Props {
   enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error('You could be a little more enthusiastic. :D');
+interface State {
+  currentEnthusiasm: number;
+}
+
+class Hello extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
   }
 
-  return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
+  updateEnthusiasm(deltaValue: number) {
+    let currentEnthusiasm = this.state.currentEnthusiasm + deltaValue;
+    if (currentEnthusiasm < 1) currentEnthusiasm = 1;
+    this.setState({
+      currentEnthusiasm,
+    });
+  }
+
+  render() {
+    const { name } = this.props;
+    const { currentEnthusiasm } = this.state;
+
+    if (currentEnthusiasm <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
+    }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(currentEnthusiasm)}
+        </div>
+        <div className="controls">
+          <button
+            onClick={() => this.updateEnthusiasm(-1)}
+            disabled={currentEnthusiasm <= 1}
+          >
+            -
+          </button>
+          <button onClick={() => this.updateEnthusiasm(1)}>+</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Hello;
