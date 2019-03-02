eb67d235f20ba22d35b8d44250895a0f3d746ffe
2alin <adilsonsdev@gmail.com>
2019-03-02 00:26:52 -0600

    add stateless function component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './components/Hello';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello name="Adilson" enthusiasmLevel={3}/>, document.getElementById('root') as HTMLElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
